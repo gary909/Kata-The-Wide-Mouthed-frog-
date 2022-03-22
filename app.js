ad9c5f1a8ad04b07e57c8a71a893ad04047a2075
gary <gary.white@lodgit.com>
2022-03-22 13:23:12 +0100

    code working

---
+++ app.js
@@ -0,0 +1,12 @@
+function mouthSize(animal) {
+    // code here
+    if (animal.toLowerCase() == "alligator") {
+        return "small";
+    } else {
+        return "wide";
+    }
+}
+
+console.log(mouthSize("toucan")); // "wide"
+console.log(mouthSize("ant bear")); // "wide"
+console.log(mouthSize("alligator")); // "small"
